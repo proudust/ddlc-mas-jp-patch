7bf03166473844648f758bf88b8cbf8c1b13f1d7
Proudust <proudust@gmail.com>
2020-02-05 21:09:40 +0900

    :wrench: 台詞の抽出時、prompt や say 直接呼び出しなどを翻訳可能文字列に追加する処理を追加

--- tools/dialogue.sh
+++ tools/dialogue.sh
@@ -11,6 +11,12 @@ if [ ! "$result" = "0" ]; then
   exit 1
 fi
 
+echo "Add mark translatable strings"
+# prompt="Thoughts on God"
+sed -i -r 's/prompt="([^"]+)"/prompt=_\("\1"\)/g' /tmp/mas-$MAS_VERSION/game/**.rpy
+# $ renpy.say(m, "What kind of weather would you like?", interact=False)
+sed -i -r 's/(renpy.say\(\w+, )"([^"]+)"/\1_\("\2"\)/g' /tmp/mas-$MAS_VERSION/game/**.rpy
+
 echo "Extract dialogs Monika After Story $MAS_VERSION"
 /tmp/renpy/renpy.sh /tmp/mas-$MAS_VERSION dialogue --strings --escape
 sed "s|`pwd`|game|" /tmp/mas-$MAS_VERSION/dialogue.tab >./dialogue.tab
