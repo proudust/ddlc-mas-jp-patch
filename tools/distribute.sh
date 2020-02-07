7aed4de2400e259706f3d08f651e927696ff499b
Proudust <proudust@gmail.com>
2020-02-07 15:07:03 +0900

    :wrench: ビルドスクリプトを微修正

--- tools/distribute.sh
+++ tools/distribute.sh
@@ -11,9 +11,14 @@ echo "Build Monika After Story JP patch ver$PATCH_VERSION for $MAS_VERSION"
 rm -rf /tmp/mas-jp
 cp -fpr /tmp/mas-$MAS_VERSION /tmp/mas-jp
 cp -fpr `dirname $0`/../patch/* /tmp/mas-jp
-/tmp/renpy/renpy.sh /tmp/renpy/launcher distribute /tmp/mas-jp --package Mod
 
-rm -rf /tmp/mas-jp-dist
+result=0
+/tmp/renpy/renpy.sh /tmp/renpy/launcher distribute /tmp/mas-jp --package Mod || result=$?
+if [ ! "$result" = "0" ]; then
+  exit 1
+fi
+
+rm -rf /tmp/mas-jp-dist ./DDLC_MAS_JP_$PATCH_VERSION ./DDLC_MAS_JP_$PATCH_VERSION.zip
 unzip -q `ls /tmp/*-dists/*-Mod.zip` -d /tmp/mas-jp-dist
 mv `ls -d /tmp/mas-jp-dist/* | head -n1` ./DDLC_MAS_JP_$PATCH_VERSION
 zip -q -r ./DDLC_MAS_JP_$PATCH_VERSION.zip ./DDLC_MAS_JP_$PATCH_VERSION
